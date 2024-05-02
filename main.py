10b48c6c74ddc15fec0a0f554c9ff70f27679f22
Albert Eksteen <alberte@acumenbi.co.nz>
2024-05-02 13:25:15 +1200

    Add folium map

--- main.py
+++ main.py
@@ -40,26 +40,63 @@ data = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
 
 st.write(data)
 
+# encode geohash from lat long 
+# geohash.encode(lat7itude = , longitude = , precision = 8)
+# returns 'qqguguga'
 
-def create_map(data):
-    # Initialize the map centered at a location
-    m = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=10)
+# decode geohash to centerpoint lat long
+# geohash.decode('qqguguga')
+# returns (latitude, longitude)
 
-    # Add markers for each data point
-    for row in data:
-        latitude = row[1]
-        longitude = row[2]
-        geohash = row[0]
-        folium.Marker([latitude, longitude], popup=geohash).add_to(m)
-        #print(f"latitude = {latitude}, longitude = {longitude}, geohash = {geohash}")
-    
-    return m
+# bounding box of the geohash
+# geohash.bbox('qqu57zr2v') 
+# returns {'s': , 'w': , 'n': , 'e': , }
 
 
+# Upper_left_point = (N, W)
+# Upper_right_point = (N, E)
+# Lower_right_point = (S, E)
+# Lower_left_point = (S, W)
 
-st.write('Map:')
-map_data = create_map(data)
-st.write(map_data)
+
+# we need to draw the map
+
+
+# lat, long = geohash.decode(‘qqu57’) # get the center of the geohash
+
+m = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=5)
+
+# create a map canvas
+m = folium.Map(
+    location=[data['latitude'].mean(),data['longitude'].mean()], # set the center location of the map
+    zoom_start=9.5,
+    tiles="CartoDB Positron"
+)
+
+# show the map
+m 
+
+
+
+#def create_map(data):
+#    # Initialize the map centered at a location
+#    m = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=10)
+#
+#    # Add markers for each data point
+#    for row in data:
+#        latitude = row[1]
+#        longitude = row[2]
+#        geohash = row[0]
+#        folium.Marker([latitude, longitude], popup=geohash).add_to(m)
+#        #print(f"latitude = {latitude}, longitude = {longitude}, geohash = {geohash}")
+#    
+#    return m
+
+
+
+#st.write('Map:')
+#map_data = create_map(data)
+#st.write(map_data)
 
 #map_data = create_map(data)
 #folium_static(map_data)
