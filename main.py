c9c873eb7c7903bdf511575f83180d6a94b6a84e
Albert Eksteen <alberte@acumenbi.co.nz>
2024-05-08 20:11:09 +1200

    Added bbox edges to map

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ import pandas as pd
 import streamlit as st
 from streamlit_folium import folium_static
 import folium
-
-
-#config = configparser.ConfigParser()
-#config.read('config.ini')
-
-
+import geohash
+from math import log10
 
 c_sf_account = st.secrets["Snowflake"]["account"]
 c_sf_username = st.secrets["Snowflake"]["username"]
@@ -28,22 +24,39 @@ conn = snowflake.connector.connect(
 
 # Create a cursor object
 cursor = conn.cursor()
-
-
-query = "SELECT GEOHASH, LATITUDE, LONGITUDE FROM STREAMLIT.SPATIAL_DATA.GEOHASH_DATA"
-
-# Execute query
+query = 'SELECT GEOHASH, LATITUDE, LONGITUDE FROM STREAMLIT.SPATIAL_DATA.GEOHASH_DATA'
 cursor.execute(query)
-
-# Fetch results
 results = cursor.fetchall()
+
 
 data = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
 st.write('Data:')
 st.write(data)
 
-def create_map(data):
 
+print(geohash.bbox('9q8zp6z4mwvg'))
+
+
+
+def geohash_bbox(geohash_value):
+    bbox_values = geohash.bbox(geohash_value)
+    W = bbox_values["w"]
+    E = bbox_values["e"]
+    N = bbox_values["n"]
+    S = bbox_values["s"]
+
+    upper_left = (N, W)
+    upper_right = (N, E)
+    lower_right = (S, E)
+    lower_left = (S, W)
+    #edges = [upper_left, upper_right, lower_right, lower_left]
+    edges = [upper_left, lower_right]
+
+    return edges
+
+print(geohash_bbox('9q8zp6z4'))
+
+def create_map(data, precision):
     mean_latitude = data[['LATITUDE']].mean()
     mean_longitude = data[['LONGITUDE']].mean()
     # Initialize the map centered at a location
@@ -53,12 +66,13 @@ def create_map(data):
     for index, row in data.iterrows():
         latitude = row['LATITUDE']
         longitude = row['LONGITUDE']
-        geohash = row['GEOHASH']
-        folium.Marker([latitude, longitude], popup=geohash).add_to(m)
+        edges = geohash_bbox(row['GEOHASH'][:precision])
+        #folium.Marker([latitude, longitude], popup=geohash).add_to(m)
+        folium.Rectangle(bounds=edges, color="blue", fill_color="green", weight=1, popup=edges).add_to(m)
     
     return m
 
 
 st.write('Map:')
-map_data = create_map(data)
+map_data = create_map(data, 6)
 folium_static(map_data)
