3fe313ed9b09d2026086bd11a9be77b9a32d9f06
Albert Eksteen <alberte@acumenbi.co.nz>
2024-07-01 14:26:19 +1200

    Updated sql statement.

--- main.py
+++ main.py
@@ -30,22 +30,17 @@ geohash_precision = st.slider("How precise do you want your Geohash to be?", 1, 
 cursor = conn.cursor()
 query = f'''
 with raw_data as (
-select  Start_Time as datetime, 
-        Start_Lat as latitude, 
-        Start_Lng as longitude,
-        left(GeoHash,{geohash_precision}) as geohash
-from    spatial_data.dbo.Accidents_All
-where   County = 'Los Angeles'
-)
-
+select  datetime, 
+        latitude, 
+        longitude,
+        left(GeoHash,6) as geohash
+from    streamlit.spatial_data.geohash_data)
 , geohash_data as (
-select  left(geohash,{geohash_precision}) as geohash,
+select  left(geohash,6) as geohash,
         count(*) as qty
-from    spatial_data.dbo.Accidents_All
-where   County = 'Los Angeles'
-group   by left(geohash,{geohash_precision})
-having	count(*) > 1000
-)
+from    streamlit.spatial_data.geohash_data
+group   by left(geohash,6)
+having  count(*) > 1000)
 select  distinct
         a.geohash,
         b.qty
