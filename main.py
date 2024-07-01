6a64720562cb187fcdddd0fb015b59bb7d5c67c7
Albert Eksteen <alberte@acumenbi.co.nz>
2024-07-01 13:54:21 +1200

    Updated SQL Code

--- main.py
+++ main.py
@@ -29,29 +29,29 @@ geohash_precision = st.slider("How precise do you want your Geohash to be?", 1, 
 # Create a cursor object
 cursor = conn.cursor()
 query = f'''
-    with raw_data as (
-    select  Start_Time as datetime, 
-            Start_Lat as latitude, 
-            Start_Lng as longitude,
-            left(GeoHash,{geohash_precision}) as geohash
-    from	[spatial_data].[dbo].[Accidents_All]
-    where	County = 'Los Angeles'
-    )
+with raw_data as (
+select  Start_Time as datetime, 
+        Start_Lat as latitude, 
+        Start_Lng as longitude,
+        left(GeoHash,{geohash_precision}) as geohash
+from	[spatial_data].[dbo].[Accidents_All]
+where	County = 'Los Angeles'
+)
 
-    , geohash_data as (
-    select  left(geohash,{geohash_precision}) as geohash,
-            count(*) as qty
-    from	[spatial_data].[dbo].[Accidents_All]
-    where	County = 'Los Angeles'
-    group   by left(geohash,{geohash_precision})
-    having	count(*) > 1000
-    )
-    select  distinct
-            a.geohash,
-            b.qty
-    from	raw_data as a
-    join	geohash_data as b on b.geohash = a.geohash
-    '''
+, geohash_data as (
+select  left(geohash,{geohash_precision}) as geohash,
+        count(*) as qty
+from	[spatial_data].[dbo].[Accidents_All]
+where	County = 'Los Angeles'
+group   by left(geohash,{geohash_precision})
+having	count(*) > 1000
+)
+select  distinct
+        a.geohash,
+        b.qty
+from	raw_data as a
+join	geohash_data as b on b.geohash = a.geohash
+'''
 cursor.execute(query)
 results = cursor.fetchall()
 
